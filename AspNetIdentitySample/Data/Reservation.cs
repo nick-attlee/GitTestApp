d958a606a2b89587e947eec1557061e6c37b8f2b
Nicholas Attlee <nicholas.attlee1@tafensw.edu.au>
2020-05-22 12:41:44 +1000

    Added initial reservation class

---
+++ AspNetIdentitySample/Data/Reservation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetIdentitySample.Data
+{
+    public class Reservation
+    {
+        public int Id { get; set; }
+        public DateTime StartTime { get; set; }
+        public short NumOfGuests { get; set; }
+        public string Instructions { get; set; }
+    }
+}
